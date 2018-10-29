c79c4c8251e10a8612255d64e00b798320eda480
Stacey P <stacey.cupid@gmail.com>
2018-10-29 22:51:36 +0000

    Done.

--- index.js
+++ index.js
@@ -1,11 +1,30 @@
 function dwarfRollCall(dwarves) {
+    var seven = [];
+  for (let i = 0; i < dwarves.length; i++) {
+    seven.push(`${[i+1]}. ${dwarves[i]} `);
+  }
+  return seven.join('')
 }
 
-function summonCaptainPlanet(planeteerCalls){
+function summonCaptainPlanet(planeteerCalls) {
+  var five = planeteerCalls.map(element => element.toUpperCase() + '!')
+return five 
 }
 
 function longPlaneteerCalls(words) {
+  if (words.length > 4) {
+    return true
+  }
+  else {
+    return false
+  }
 }
 
 function findTheCheese (foods) {
+  if (foods.includes('cheddar')) {
+    return 'cheddar'
+  }
+  else {
+    return 'no cheese!'
+  }
 }
